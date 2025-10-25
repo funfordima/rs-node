093c674e6d243441e9dbfff547f8019b6c573adb
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-10-25 13:06:22 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { rename as renameFile } from 'fs/promises';
+
+import { BASE_ERROR, FILES } from '../constants/pathDirectory.js';
+import { getFilePath } from '../helpers/getFilePath.js';
+import { checkFileExists } from '../helpers/checkFileExists.js';
+
 const rename = async () => {
-  // Write your code here
+    const sourceFileName = 'wrongFilename.txt';
+    const targetFileName = 'properFilename.md';
+    const sourceFilePath = path.resolve(getFilePath(import.meta.url), FILES, sourceFileName);
+    const targetFilePath = path.resolve(getFilePath(import.meta.url), FILES, targetFileName);
+    const isSourceFileExists = await checkFileExists(sourceFilePath);
+    const isTargetFileExists = await checkFileExists(targetFilePath);
+  
+    if (!isSourceFileExists || isTargetFileExists) {
+      throw Error(BASE_ERROR);
+    }
+  
+    try {
+      await renameFile(sourceFilePath, targetFilePath);
+    } catch (error) {
+      process.stderr.write(error);
+    }
 };
 
 await rename();
