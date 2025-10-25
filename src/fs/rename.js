4e6df0e333ffbd664414803871b0d0eefe194ecd
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-10-25 13:21:44 +0300

    feat: implement list

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -6,22 +6,22 @@ import { getFilePath } from '../helpers/getFilePath.js';
 import { checkFileExists } from '../helpers/checkFileExists.js';
 
 const rename = async () => {
-    const sourceFileName = 'wrongFilename.txt';
-    const targetFileName = 'properFilename.md';
-    const sourceFilePath = path.resolve(getFilePath(import.meta.url), FILES, sourceFileName);
-    const targetFilePath = path.resolve(getFilePath(import.meta.url), FILES, targetFileName);
-    const isSourceFileExists = await checkFileExists(sourceFilePath);
-    const isTargetFileExists = await checkFileExists(targetFilePath);
-  
-    if (!isSourceFileExists || isTargetFileExists) {
-      throw Error(BASE_ERROR);
-    }
-  
-    try {
-      await renameFile(sourceFilePath, targetFilePath);
-    } catch (error) {
-      process.stderr.write(error);
-    }
+  const sourceFileName = 'wrongFilename.txt';
+  const targetFileName = 'properFilename.md';
+  const sourceFilePath = path.resolve(getFilePath(import.meta.url), FILES, sourceFileName);
+  const targetFilePath = path.resolve(getFilePath(import.meta.url), FILES, targetFileName);
+  const isSourceFileExists = await checkFileExists(sourceFilePath);
+  const isTargetFileExists = await checkFileExists(targetFilePath);
+
+  if (!isSourceFileExists || isTargetFileExists) {
+    throw Error(BASE_ERROR);
+  }
+
+  try {
+    await renameFile(sourceFilePath, targetFilePath);
+  } catch (error) {
+    process.stderr.write(error);
+  }
 };
 
 await rename();
