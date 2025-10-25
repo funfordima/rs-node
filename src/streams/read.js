335e16fc63609963c591fd4d403c33344d37074b
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-10-25 19:21:27 +0300

    feat: implement read/write streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,39 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { Writable } from 'stream';
+import { pipeline } from 'stream/promises';
+
+import { BASE_ERROR, FILES } from '../constants/pathDirectory.js';
+import { getFilePath } from '../helpers/getFilePath.js';
+import { checkFileExists } from '../helpers/checkFileExists.js';
+
 const read = async () => {
-  // Write your code here
+  const fileName = 'fileToRead.txt';
+  const filePath = path.resolve(getFilePath(import.meta.url), FILES, fileName);
+  const isFileExists = await checkFileExists(filePath);
+  
+  if (!isFileExists) {
+    throw Error(BASE_ERROR);
+  }
+  
+  const input = createReadStream(filePath, { encoding: 'utf-8'});
+
+  const stdoutWrapper = new Writable({
+    write(chunk, encoding, callback) {
+      process.stdout.write(chunk, encoding, () => setImmediate(callback));
+    }
+  });
+
+  try {
+    await pipeline(
+      input, 
+      stdoutWrapper
+    );
+
+    process.stdout.write('\n');
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+import { BASE_ERROR, FILES } from '../constants/pathDirectory.js';
+import { getFilePath } from '../helpers/getFilePath.js';
+import { checkFileExists } from '../helpers/checkFileExists.js';
+
 const write = async () => {
-  // Write your code here
+  const fileName = 'fileToWrite.txt';
+    const filePath = path.resolve(getFilePath(import.meta.url), FILES, fileName);
+    const isFileExists = await checkFileExists(filePath);
+    
+    if (!isFileExists) {
+      throw Error(BASE_ERROR);
+    }
+    
+    const output = createWriteStream(filePath, { encoding: 'utf-8'});
+  
+    try {
+      await pipeline(
+        process.stdin, 
+        output
+      );
+  
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 await write();
