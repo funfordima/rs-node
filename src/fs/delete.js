32006b21675a57586d7f19df87bb26126d5a7c00
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-10-25 13:09:48 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { rm } from 'fs/promises';
+
+import { BASE_ERROR, FILES } from '../constants/pathDirectory.js';
+import { getFilePath } from '../helpers/getFilePath.js';
+import { checkFileExists } from '../helpers/checkFileExists.js';
+
 const remove = async () => {
-  // Write your code here
+  const fileName = 'fileToRemove.txt';
+  const filePath = path.resolve(getFilePath(import.meta.url), FILES, fileName);
+  const isFileExists = await checkFileExists(filePath);
+
+  if (!isFileExists) {
+    throw Error(BASE_ERROR);
+  }
+
+  try {
+    await rm(filePath);
+  } catch (error) {
+    process.stderr.write(error);
+  }
 };
 
 await remove();
