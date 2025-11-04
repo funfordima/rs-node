9d8aeda13b0f2e53729bb10d8045a8707b1c8a84
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-11-04 22:51:54 +0200

    feat: implement test case 1 and 2

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,85 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const testList = [-5, 0, 5];
+    const parameter = 2;
+    const resultList = [-3, 2, 7];
+
+    testList.forEach((n, i) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Add });
+
+      expect(result).toBe(resultList[i]);
+    });
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const testList = [-5, 0, 5];
+    const parameter = 2;
+    const resultList = [-7, -2, 3];
+
+    testList.forEach((n, i) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Subtract });
+
+      expect(result).toBe(resultList[i]);
+    });
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const testList = [-5, 0, 5];
+    const parameter = 2;
+    const resultList = [-10, 0, 10];
+
+    testList.forEach((n, i) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Multiply });
+
+      expect(result).toBe(resultList[i]);
+    });
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const testList = [-6, 0, 6];
+    const parameter = 2;
+    const resultList = [-3, 0, 3];
+
+    testList.forEach((n, i) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Divide });
+
+      expect(result).toBe(resultList[i]);
+    });
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const testList = [-5, 0, 5];
+    const parameter = 2;
+    const resultList = [25, 0, 25];
+
+    testList.forEach((n, i) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Exponentiate });
+
+      expect(result).toBe(resultList[i]);
+    });
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const testList = [-5];
+    const parameter = 2;
+
+    testList.forEach((n) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: 'InvalidAction' });
+
+      expect(result).toBe(null);
+    });
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const testList = [-5];
+    const parameter = 'number';
+
+    testList.forEach((n) => {
+      const result = simpleCalculator({ a: n, b: parameter, action: Action.Add });
+
+      expect(result).toBe(null);
+    });
   });
 });
