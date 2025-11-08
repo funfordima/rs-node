367793b150bd1c1e527e60594fa4160067ef876c
DmytroLitvinov <d.g.litvinov@gmail.com>
2025-11-08 12:41:27 +0200

    feat: create simple http server

---
+++ src/constants/methods.ts
@@ -0,0 +1,6 @@
+export enum MethodsEnum {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log('hello!');
-console.log('hello!');
+import 'dotenv/config';
+import { createServer } from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = createServer((req, res) => {
+  try {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      data: 'Hello World!',
+    }));
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'An internal server error occurred' }));
+  }
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default server;
